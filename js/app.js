68e14025137870ad2329f6dc2da4f5e972223bdf
mmoya26 <mmoya18@icloud.com>
2021-03-22 21:59:01 -0500

    Add MLibrary project + Finish JavaScript for side_bar + Plus update overlay when side_bar is activated

--- js/app.js
+++ js/app.js
@@ -13,20 +13,25 @@ let body = document.querySelector("body");
 // Get Li's inside side navbar
 let side_bar_list_items = document.querySelector("#side_bar li");
 
+// Overlay element
+let overlayScreen = document.querySelector('.overlay');
+
+// Side bar links
+let sideBarItems = Array.from(document.querySelectorAll('.side_bar li a'));
+
 bars.addEventListener("click", () => {
-
-    // Remove display_none class from side_bar
-    side_bar.classList.remove("display_none");
-
-    // Add background_blur class to body
-    body.classList.add("background_blur");
+    side_bar.classList.toggle("display_none");
+    overlayScreen.classList.toggle('display_none');
 });
 
 x_icon.addEventListener("click", () => {
-
-    // Add display_none class to side_bar
-    side_bar.classList.add("display_none");
-
-    // Remove background_blur class to body
-    body.classList.remove("background_blur");
+    side_bar.classList.toggle("display_none");
+    overlayScreen.classList.toggle('display_none');
 });
+
+sideBarItems.forEach(item => {
+    item.addEventListener("click", (e) => {
+        side_bar.classList.toggle("display_none");
+        overlayScreen.classList.toggle('display_none');
+    });
+})
