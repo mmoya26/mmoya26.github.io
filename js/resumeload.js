4c5175b7cfd8bf8d4de1f8723179c0d4cac402de
mmoya26 <mmoya18@icloud.com>
2023-01-18 13:31:57 -0400

    Added New Resume + Removed console.log from resumeload.js

--- js/resumeload.js
+++ js/resumeload.js
@@ -1,8 +1,7 @@
 window.onload = () => {
-  const resumeLink = "https://drive.google.com/file/d/1i2Ir6bT4CGQx9XRROPHGxelmEObQ_dBh/view?usp=sharing";
+  const resumeLink = "../resume/2023_CV.pdf";
   const resumeButtons = Array.from(document.querySelectorAll(".btn_resume"));
   resumeButtons.forEach(element => {
     element.href = resumeLink;
-    console.log("Resume Link Updated");
   });
 }
